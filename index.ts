d520e32376f755c1be4dffef864fde5d18089254
Maciej Smolinski <contact@maciejsmolinski.com>
2021-09-29 23:11:20 +0200

    refactor: rename classes and class properties
    As the code evolved, the classes kept their irrelevant names.
    This commit changes that.

--- index.ts
+++ index.ts
@@ -24,6 +24,116 @@ interface Token {
   value?: any;
 }
 
+class Scanner {
+  pos: number;
+  text: string;
+
+  constructor(text: string) {
+    this.pos = -1;
+    this.text = text;
+  }
+
+  read() {
+    if (this.pos + 1 > this.text.length) {
+      return '';
+    }
+
+    return this.text[++this.pos];
+  }
+
+  peek() {
+    return this.text[this.pos + 1] || '';
+  }
+
+  consume(...chars: string[]) {
+    while (this.peek() && chars.includes(this.peek())) {
+      this.read();
+    }
+  }
+}
+
+class Lexer {
+  scanner: Scanner;
+
+  constructor(scanner: Scanner) {
+    this.scanner = scanner;
+  }
+
+  private isNumeric(char: string): boolean {
+    return /\d/.test(char);
+  }
+
+  private isCharacter(char: string): boolean {
+    return /\w/.test(char);
+  }
+
+  next(): Token | null {
+    this.scanner.consume('\n', ' ');
+
+    if (this.scanner.peek() === '') {
+      return null;
+    }
+
+    if (this.isNumeric(this.scanner.peek())) {
+      let number = this.scanner.read();
+
+      while (this.isNumeric(this.scanner.peek())) {
+        number += this.scanner.read();
+      }
+
+      if (this.scanner.peek() === '.') {
+        number += this.scanner.read();
+      }
+
+      while (this.isNumeric(this.scanner.peek())) {
+        number += this.scanner.read();
+      }
+
+      return { type: 'number', value: Number(number) };
+    }
+
+    if (this.isCharacter(this.scanner.peek())) {
+      let string = '';
+      let token: Token;
+
+      while (this.isCharacter(this.scanner.peek())) {
+        string += this.scanner.read();
+
+        if (string === 'return') {
+          token = { type: 'return' };
+          break;
+        }
+      }
+
+      token ||= { type: 'string', value: string };
+
+      return token;
+    }
+
+    return null;
+  }
+}
+
+function tokenize(text: string) {
+  const lexer = new Scanner(text);
+  const tokens = new Lexer(lexer);
+  let token;
+
+  console.log(`\n\n[INPUT]\n\n${text}\n`);
+
+  console.log('[OUTPUT]\n');
+
+  while ((token = tokens.next())) {
+    console.log(token);
+  }
+}
+
+tokenize('  123');
+tokenize('  321.123');
+tokenize('  321....');
+tokenize('return 123');
+tokenize(program);
+
 // const tokens: Token[] = [
 //   { type: 'fun' },
 //   { type: 'identifier', value: 'average' },
@@ -48,115 +158,3 @@ interface Token {
 //   { type: 'number', value: 2 },
 //   { type: 'rightParen' },
 // ];
-
-class Lexer {
-  pos: number;
-  text: string;
-
-  constructor(text: string) {
-    this.pos = -1;
-    this.text = text;
-  }
-
-  read() {
-    if (this.pos + 1 > this.text.length) {
-      return '';
-    }
-
-    return this.text[++this.pos]; // || '';
-  }
-
-  peek() {
-    if (this.pos + 1 === this.text.length) {
-      return '';
-    }
-
-    return this.text[this.pos + 1]; // || '';
-  }
-
-  consume(...chars: string[]) {
-    while (this.peek() && chars.includes(this.peek())) {
-      this.read();
-    }
-  }
-}
-
-class TokenStream {
-  lexer: Lexer;
-
-  constructor(lexer: Lexer) {
-    this.lexer = lexer;
-  }
-
-  private isNumeric(char: string): boolean {
-    return /\d/.test(char);
-  }
-
-  private isCharacter(char: string): boolean {
-    return /\w/.test(char);
-  }
-
-  next(): Token | null {
-    this.lexer.consume('\n', ' ');
-
-    if (this.lexer.peek() === '') {
-      return null;
-    }
-
-    if (this.isNumeric(this.lexer.peek())) {
-      let number = this.lexer.read();
-
-      while (this.isNumeric(this.lexer.peek())) {
-        number += this.lexer.read();
-      }
-
-      if (this.lexer.peek() === '.') {
-        number += this.lexer.read();
-      }
-
-      while (this.isNumeric(this.lexer.peek())) {
-        number += this.lexer.read();
-      }
-
-      return { type: 'number', value: Number(number) };
-    }
-
-    if (this.isCharacter(this.lexer.peek())) {
-      let string = '';
-      let token: Token;
-
-      while (this.isCharacter(this.lexer.peek())) {
-        string += this.lexer.read();
-
-        if (string === 'return') {
-          token = { type: 'return' };
-          break;
-        }
-      }
-
-      token ||= { type: 'string', value: string };
-
-      return token;
-    }
-
-    return null;
-  }
-}
-
-function tokenize(text: string) {
-  const lexer = new Lexer(text);
-  const tokens = new TokenStream(lexer);
-  let token;
-
-  console.log(`[SCAN] ${text}`);
-
-  while ((token = tokens.next())) {
-    console.log(token);
-  }
-}
-
-tokenize('  123');
-tokenize('  321.123');
-tokenize('  321....');
-tokenize('return 123');
-tokenize(program);
