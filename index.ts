0efa8ecd94b0ea04c0c41666641e77f83f438098
Maciej Smolinski <contact@maciejsmolinski.com>
2021-10-01 01:09:43 +0200

    feat: log lexing and parsing output

--- index.ts
+++ index.ts
@@ -13,10 +13,6 @@ function tokenize(text: string) {
   const lexer = new Lexer(scanner);
   let token;
 
-  console.log(`\n\n[INPUT]\n\n${text}\n`);
-
-  console.log('[OUTPUT]\n');
-
   while ((token = lexer.next())) {
     console.log(token);
   }
@@ -27,12 +23,17 @@ function parse(text: string) {
   const lexer = new Lexer(scanner);
   const parser = new Parser(lexer);
 
-  console.log(`\n\n[INPUT]\n\n${text}\n`);
-
-  console.log('[OUTPUT]\n');
-
   console.log(JSON.stringify(parser.parse(), null, 2));
 }
 
-tokenize(program);
-// parse(program);
+function main(text: string) {
+  console.log(`\n\n[INPUT]\n\n${text}`);
+
+  console.log('\n\n[TOKENIZE:OUTPUT]\n');
+  tokenize(text);
+
+  console.log('\n\n[PARSE:OUTPUT]\n');
+  parse(text);
+}
+
+main(program);
